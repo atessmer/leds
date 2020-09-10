a01b981ee82e86adbc40251f2e046bd64bb251b1
Alex Tessmer <atessmer@vmware.com>
2020-09-09 22:52:45 -0500

    Add a WaveChain class
    Make the code for constructing the wave chain data more readable
    by adding a WaveChain class with methods for each command.

--- virtual_wall.py
+++ virtual_wall.py
@@ -13,6 +13,42 @@ def khz_to_usec(khz):
     return int((1/khz) * 1000)
 
 
+class WaveChain:
+    # Documentation at http://abyz.me.uk/rpi/pigpio/python.html#wave_chain
+
+    @staticmethod
+    def _decompose_cmd_count(n):
+        """ Convert a command count to the x,y form used by wave chains """
+        x = n % 256
+        y = int(n / 256)
+        return [x, y]
+
+    def __init__(self, pi):
+        self.pi = pi
+        self.data = []
+
+    def start_loop(self):
+        """ Start a new loop """
+        self.data += [255, 0]
+
+    def end_loop(self, repeat):
+        """ End the current loop after 'repeat' iterations """
+        self.data += [255, 1] + self._decompose_cmd_count(repeat)
+
+    def delay(self, duration):
+        """ Add a 'duration' microsecond delay """
+        self.data += [255, 2] + self._decompose_cmd_count(duration)
+
+    def loop_forever(self):
+        self.data += [255, 3]
+
+    def transmit_wave(self, wave_id):
+        self.data += [wave_id]
+
+    def start(self):
+        self.pi.wave_chain(self.data)
+
+
 def main():
     pi = pigpio.pi()
     pi.set_mode(GPIO, pigpio.OUTPUT)
@@ -23,16 +59,17 @@ def main():
         pigpio.pulse(1<<GPIO, 0, khz_to_usec(FREQUENCY*2)),
         pigpio.pulse(0, 1<<GPIO, khz_to_usec(FREQUENCY*2)),
     ])
-    wid_mark = pi.wave_create()
+    mark_usec = pi.wave_get_micros()
+    mark_wid = pi.wave_create()
 
-    pi.wave_chain([
-        255, 0,             # loop (A) start
-        255, 0,             # loop (B) start
-        wid_mark,           # transmit mark wave
-        255, 1, 38, 0,      # loop (B) end: 38 times; 38 * 26us = ~ 1000 usec
-        255, 2, 232, 3,     # delay 1000 usec
-        255, 3,             # loop (A) repeat forever
-    ])
+    wc = WaveChain(pi)
+    wc.start_loop()                     # loop (A)
+    wc.start_loop()                     # loop (B)
+    wc.transmit_wave(mark_wid)
+    wc.end_loop(int(1000/mark_usec))    # loop (B); ~1000 usec
+    wc.delay(1000)
+    wc.loop_forever()                   # loop (A)
+    wc.start()
 
     try:
         while pi.wave_tx_busy():
@@ -40,8 +77,7 @@ def main():
     except KeyboardInterrupt:
         pi.wave_tx_stop()
 
-    pi.wave_delete(wid_mark)
-    pi.wave_delete(wid_space)
+    pi.wave_delete(mark_wid)
 
 
 if __name__ == '__main__':
